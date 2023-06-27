8c472466d1e0fb45539db81bf0db2417dff3d147
lckdscl <lckdscl+github@protonmail.com>
2023-06-27 15:05:43 +0100

    New file: plugins for markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,35 @@
+-- Plugins that add functionalities to markdown files
+return {
+	{
+		-- Format tables in markdown files
+		"godlygeek/tabular",
+		lazy = true,
+		dependencies = { "vim-markdown" },
+		ft = { "markdown" },
+	},
+	{
+		-- Preview markdown in browser
+		"iamcco/markdown-preview.nvim",
+		lazy = true,
+		cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		config = function()
+			require("configs.mdpreview")
+		end,
+	},
+	{
+		-- Display wordcount under section header
+		"dimfeld/section-wordcount.nvim",
+		lazy = true,
+		ft = { "markdown" },
+		config = function()
+			require("section-wordcount").setup({
+				-- These are the default values and can be omitted
+				highlight = "NonText",
+				virt_text_pos = "eol",
+			})
+		end,
+	},
+}
