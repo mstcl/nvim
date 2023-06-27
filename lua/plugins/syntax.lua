027c9583fc3c87dfbdfaeae015f60a03ad40196b
lckdscl <lckdscl+github@protonmail.com>
2023-06-27 15:06:24 +0100

    Added: captions and reorganised

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,5 +1,7 @@
+-- Plugins to extend syntax, either natively or with treesitter
 return {
 	{
+		-- Treesitter engine
 		"nvim-treesitter/nvim-treesitter",
 		lazy = true,
 		event = { "BufWinEnter" },
@@ -22,5 +24,44 @@ return {
 			},
 		},
 	},
-	-- { "nvim-treesitter/nvim-treesitter-context", event = "BufRead" }
+	{
+		-- Concealing in tex
+		"KeitaNakamura/tex-conceal.vim",
+		lazy = true,
+		ft = { "tex" },
+	},
+	{
+		-- Markdown syntax
+		"hhn-pham/vim-markdown",
+		lazy = true,
+		branch = "main",
+		ft = { "markdown" },
+		init = function()
+			require("configs.markdown")
+		end,
+		wants = "tabular",
+	},
+	{
+		-- SXHKD syntax
+		"kovetskiy/sxhkd-vim",
+		lazy = true,
+		ft = "sxhkd",
+	},
+	{
+		-- Orgmode syntax
+		"nvim-orgmode/orgmode",
+		config = function()
+			require("orgmode").setup({
+				org_agenda_files = { "~/sftpgo/orgzly/Main.org", "~/sftpgo/shared/orgzly/*" },
+				win_split_mode = "float",
+			})
+			require("orgmode").setup_ts_grammar()
+		end,
+	},
+	{
+		-- Dokuwiki syntax
+		"nblock/vim-dokuwiki",
+		lazy = true,
+		ft = "dokuwiki",
+	},
 }
