d24808af07b830965d6d2f87ed90d9b91f789304
lckdscl <lckdscl@bim.boats>
2023-07-25 12:23:32 +0100

    Added: new ft

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,83 +1,105 @@
 local present, ts = pcall(require, "nvim-treesitter.configs")
 if not present then
-    return
+	return
 end
 
 local rainbow = require("ts-rainbow")
 
 ts.setup({
-    ensure_installed = {
-        "python",
-        "html",
-        "c",
-        "cpp",
-        "bibtex",
-        "css",
-        "json",
-        "vim",
-        "java",
-        "javascript",
-        "cmake",
-        -- "latex",
-        "lua",
-        "bash",
-        "toml",
-        "comment",
-        "org",
-        "rust",
-        "go",
+	ensure_installed = {
+		"python",
+		"html",
+		"c",
+		"cpp",
+		"bibtex",
+		"css",
+		"json",
+		"vim",
+		"java",
+		"javascript",
+		"cmake",
+		"latex",
+		"lua",
+		"bash",
+		"diff",
+		"dockerfile",
+		"gitignore",
+		"ini",
+		"passwd",
+		"php",
+		"rasi",
+		"scss",
+		"sxhkdrc",
+		"yaml",
+		"toml",
+		"comment",
+		"org",
+		"rust",
+		"go",
+	},
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+		additional_vim_regex_highlighting = { "org" },
+		disable = { "latex", "markdown" },
+	},
+	autopairs = {
+		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	rainbow = {
+		enable = true,
+		query = {
+			"rainbow-parens",
+			html = "rainbow-tags",
+			latex = "rainbow-blocks",
+		},
+		strategy = {
+			rainbow.strategy["global"],
+			html = rainbow.strategy["local"],
+			latex = function()
+				if vim.fn.line("$") > 10000 then
+					return nil
+				elseif vim.fn.line("$") > 1000 then
+					return rainbow.strategy["global"]
+				end
+				return rainbow.strategy["local"]
+			end,
+		},
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+    indent = {
+        enable = true
     },
-    highlight = {
-        enable = true,
-        use_languagetree = true,
-        additional_vim_regex_highlighting = {'org'},
-        disable = { "latex", "markdown" },
-    },
-    autopairs = {
-        enable = true,
-    },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
-    rainbow = {
-        enable = true,
-        query = {
-            "rainbow-parens",
-            html = "rainbow-tags",
-            latex = "rainbow-blocks",
-        },
-        strategy = {
-            rainbow.strategy["global"],
-            html = rainbow.strategy["local"],
-            latex = function()
-                if vim.fn.line("$") > 10000 then
-                    return nil
-                elseif vim.fn.line("$") > 1000 then
-                    return rainbow.strategy["global"]
-                end
-                return rainbow.strategy["local"]
-            end,
-        },
-    },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?",
-        },
-    },
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
 })
 
 vim.cmd("autocmd Filetype * silent! TSEnableAll rainbow")
