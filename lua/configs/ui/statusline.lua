1f6063e02c86de3f1c523f7997544e699c31f61f
lckdscl <lckdscl@bim.boats>
2023-09-05 22:17:08 +0100

    fix (statusline): modified and read-only icons

--- lua/configs/ui/statusline.lua
+++ lua/configs/ui/statusline.lua
@@ -3,276 +3,266 @@
 local gl = require("galaxyline")
 local condition = require("galaxyline.condition")
 local gls = gl.section
-local search = require("galaxyline.providers.search")
 local noice = require("noice")
-SearchResults = search.get_results
+local fileinfo = require('galaxyline.provider_fileinfo')
 
 gl.short_line_list = {
-	"lazy",
-	"Trouble",
-	"LuaTree",
-	"dapui_scopes",
-	"dapui_breakpoints",
-	"dapui_repl",
-	"vista",
-	"qf",
-	"dbui",
-	"startify",
-	"",
-	"floaterm",
-	"DiffviewFileHistoryPanel",
-	"DiffviewFiles",
-	"alpha",
-	"man",
-	"term",
-	"nerdtree",
-	"dashboard",
-	"Packer",
-	"NvimTree",
-	"TelescopePrompt",
-	"Help",
-	"diff",
-	"undotree",
-	"netrw",
-	"plug",
-	"Outline",
-	"quickfix",
-	"toggleterm",
+    "lazy",
+    "Trouble",
+    "LuaTree",
+    "dapui_scopes",
+    "dapui_breakpoints",
+    "dapui_repl",
+    "vista",
+    "qf",
+    "dbui",
+    "startify",
+    "",
+    "floaterm",
+    "DiffviewFileHistoryPanel",
+    "DiffviewFiles",
+    "alpha",
+    "man",
+    "term",
+    "nerdtree",
+    "dashboard",
+    "Packer",
+    "NvimTree",
+    "TelescopePrompt",
+    "Help",
+    "diff",
+    "undotree",
+    "netrw",
+    "plug",
+    "Outline",
+    "quickfix",
+    "toggleterm",
 }
 local colors = {
-	normal = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("TelescopeSelection", true).foreground)),
-	insert = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Statement", true).foreground)),
-	visual = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Constant", true).foreground)),
-	cmd = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Conditional", true).foreground)),
-	replace = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Exception", true).foreground)),
-	term = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Warning", true).background)),
+    normal = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("TelescopeSelection", true).foreground)),
+    insert = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Statement", true).foreground)),
+    visual = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Constant", true).foreground)),
+    cmd = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Conditional", true).foreground)),
+    replace = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Exception", true).foreground)),
+    term = "#" .. tostring(string.format("%06x", vim.api.nvim_get_hl_by_name("Warning", true).background)),
 }
 
 local vi_mode = function()
-	local alias = {
-		n = "NVM",
-		i = "INS",
-		c = "CMD",
-		V = "VIS",
-		[""] = "VIS",
-		v = "VIS",
-		c = "CMD",
-		["r?"] = ":CONFIRM",
-		rm = "--MORE",
-		R = "REP",
-		Rv = "VIR",
-		s = "SEL",
-		S = "SEL",
-		["r"] = "HIT-ENTER",
-		[""] = "SEL",
-		t = "TERM",
-		["!"] = "SHELL",
-	}
-	local mode_color = {
-		n = colors.normal,
-		i = colors.insert,
-		v = colors.visual,
-		[""] = colors.visual,
-		V = colors.visual,
-		c = colors.cmd,
-		s = colors.replace,
-		S = colors.replace,
-		[""] = colors.replace,
-		ic = colors.insert,
-		R = colors.replace,
-		Rv = colors.replace,
-		cv = colors.replace,
-		ce = colors.replace,
-		r = colors.replace,
-		rm = colors.visual,
-		["r?"] = colors.visual,
-		["!"] = colors.replace,
-		t = colors.term,
-	}
-	local vim_mode = vim.fn.mode()
-	vim.api.nvim_command("hi GalaxyViMode gui=bold guifg=#f5f5f5 guibg=" .. mode_color[vim.fn.mode()])
-	return "  " .. alias[vim_mode] .. " "
+    local alias = {
+        n = "NVM",
+        i = "INS",
+        c = "CMD",
+        V = "VIS",
+        [""] = "VIS",
+        v = "VIS",
+        c = "CMD",
+        ["r?"] = ":CONFIRM",
+        rm = "--MORE",
+        R = "REP",
+        Rv = "VIR",
+        s = "SEL",
+        S = "SEL",
+        ["r"] = "HIT-ENTER",
+        [""] = "SEL",
+        t = "TERM",
+        ["!"] = "SHELL",
+    }
+    local mode_color = {
+        n = colors.normal,
+        i = colors.insert,
+        v = colors.visual,
+        [""] = colors.visual,
+        V = colors.visual,
+        c = colors.cmd,
+        s = colors.replace,
+        S = colors.replace,
+        [""] = colors.replace,
+        ic = colors.insert,
+        R = colors.replace,
+        Rv = colors.replace,
+        cv = colors.replace,
+        ce = colors.replace,
+        r = colors.replace,
+        rm = colors.visual,
+        ["r?"] = colors.visual,
+        ["!"] = colors.replace,
+        t = colors.term,
+    }
+    local vim_mode = vim.fn.mode()
+    vim.api.nvim_command("hi GalaxyViMode gui=bold guifg=#f5f5f5 guibg=" .. mode_color[vim.fn.mode()])
+    return "  " .. alias[vim_mode] .. " "
 end
 
 gls.left[2] = {
-	ViMode = {
-		provider = function()
-			return vi_mode()
-		end,
-		highlight = "GalaxyFg",
-	},
+    ViMode = {
+        provider = function()
+            return vi_mode()
+        end,
+        highlight = "GalaxyFg",
+    },
 }
 
 gls.left[5] = {
-	FileName = {
-		provider = "FileName",
-		condition = condition.buffer_not_empty,
-		highlight = "GalaxyFgAlt",
-		icon = " ",
-	},
+    FileName = {
+        provider = function()
+            return fileinfo.get_current_file_name("●","✗")
+        end,
+        condition = condition.buffer_not_empty,
+        highlight = "GalaxyFgAlt",
+        icon = " ",
+    },
 }
 
 gls.left[9] = {
-	BranchSpace = {
-		provider = function()
-			return " "
-		end,
-		highlight = "GalaxyFgAlt2",
-	},
+    BranchSpace = {
+        provider = function()
+            return " "
+        end,
+        highlight = "GalaxyFgAlt2",
+    },
 }
 
 gls.left[10] = {
-	DiffAdd = {
-		provider = "DiffAdd",
-		icon = " +",
-		highlight = "GalaxyGreen",
-	},
+    DiffAdd = {
+        provider = "DiffAdd",
+        icon = " +",
+        highlight = "GalaxyGreen",
+    },
 }
 
 gls.left[11] = {
-	DiffModified = {
-		provider = "DiffModified",
-		icon = " ~",
-		highlight = "GalaxyMagenta",
-	},
+    DiffModified = {
+        provider = "DiffModified",
+        icon = " ~",
+        highlight = "GalaxyMagenta",
+    },
 }
 
 gls.left[12] = {
-	DiffRemove = {
-		provider = "DiffRemove",
-		icon = " -",
-		highlight = "GalaxyRed",
-	},
+    DiffRemove = {
+        provider = "DiffRemove",
+        icon = " -",
+        highlight = "GalaxyRed",
+    },
 }
 
 gls.left[13] = {
-	GitIcon = {
-		provider = function()
-			return " ∫"
-		end,
-		condition = condition.check_git_workspace,
-		separator = " ",
-		separator_highlight = "GalaxyFgAlt2",
-		highlight = "GalaxyFgAlt2",
-	},
+    GitIcon = {
+        provider = function()
+            return " ∫"
+        end,
+        condition = condition.check_git_workspace,
+        separator = " ",
+        separator_highlight = "GalaxyFgAlt2",
+        highlight = "GalaxyFgAlt2",
+    },
 }
 
 gls.left[14] = {
-	GitBranch = {
-		provider = "GitBranch",
-		condition = condition.check_git_workspace,
-		highlight = "GalaxyFgAlt2",
-	},
-}
-
-gls.mid[1] = {
-	Cmd = {
-		provider = function()
-			return noice.api.statusline.command.get()
-		end,
-		condition = noice.api.statusline.command.has(),
-		icon = "  ⟨",
-		highlight = "GalaxyRed",
-	},
-}
-
-gls.mid[2] = {
-	PaddingMid1 = {
-		provider = function()
-			return "⟩"
-		end,
-		condition = noice.api.statusline.command.has(),
-		highlight = "GalaxyRed",
-	},
+    GitBranch = {
+        provider = "GitBranch",
+        condition = condition.check_git_workspace,
+        highlight = "GalaxyFgAlt2",
+    },
 }
 
 gls.right[7] = {
-	DiagnosticError = {
-		provider = "DiagnosticError",
-		icon = " ✗",
-		highlight = "GalaxyRed",
-	},
+    DiagnosticError = {
+        provider = "DiagnosticError",
+        icon = " ✗",
+        highlight = "GalaxyRed",
+    },
 }
 
 gls.right[8] = {
-	DiagnosticWarn = {
-		provider = "DiagnosticWarn",
-		icon = " !",
-		highlight = "GalaxyOrange",
-	},
+    DiagnosticWarn = {
+        provider = "DiagnosticWarn",
+        icon = " !",
+        highlight = "GalaxyOrange",
+    },
 }
 
 gls.right[9] = {
-	DiagnosticHint = {
-		provider = "DiagnosticHint",
-		icon = " ?",
-		highlight = "GalaxyYellow",
-	},
+    DiagnosticHint = {
+        provider = "DiagnosticHint",
+        icon = " ?",
+        highlight = "GalaxyYellow",
+    },
 }
 
 gls.right[10] = {
-	DiagnosticInfo = {
-		provider = "DiagnosticInfo",
-		icon = " i",
-		highlight = "GalaxyBlue",
-	},
+    DiagnosticInfo = {
+        provider = "DiagnosticInfo",
+        icon = " i",
+        highlight = "GalaxyBlue",
+    },
 }
 
 gls.right[13] = {
-	LineInfo = {
-		provider = "LineColumn",
-		separator = "",
-		separator_highlight = "GalaxyFgAlt2",
-		highlight = "GalaxyFgAlt2",
-		icon = "⊕ ",
-	},
+    LineInfo = {
+        provider = "LineColumn",
+        separator = "",
+        separator_highlight = "GalaxyFgAlt2",
+        highlight = "GalaxyFgAlt2",
+        icon = "⊕ ",
+    },
 }
 
 gls.right[14] = {
-	LinePercent = {
-		separator = "@",
-		separator_highlight = "GalaxyFgAlt2",
-		provider = "LinePercent",
-		highlight = "GalaxyFgAlt2",
-	},
+    LinePercent = {
+        separator = "@",
+        separator_highlight = "GalaxyFgAlt2",
+        provider = "LinePercent",
+        highlight = "GalaxyFgAlt2",
+    },
 }
 
 gls.right[15] = {
-	Macro = {
-		provider = function()
-			return noice.api.statusline.mode.get()
-		end,
-		condition = noice.api.status.mode.has(),
-		icon = " ❖ ",
-		highlight = "GalaxyGreen",
-	},
+    Macro = {
+        provider = function()
+            return noice.api.statusline.mode.get()
+        end,
+        condition = noice.api.status.mode.has(),
+        icon = " ❖ ",
+        highlight = "GalaxyGreen",
+    },
 }
 
 gls.right[16] = {
-	Search = {
-		provider = function()
-			return noice.api.statusline.search.get()
-		end,
-		condition = noice.api.statusline.search.has(),
-		icon = " ∊ ",
-		highlight = "GalaxyBlue",
-	},
+    Whitespace = {
+        provider = "WhiteSpace",
+        icon = " ⇆ ",
+        separator = "",
+        separator_highlight = "GalaxyFgAlt2",
+        highlight = "GalaxyYellow",
+    },
+}
+
+gls.right[17] = {
+    Search = {
+        provider = function()
+            return noice.api.statusline.search.get()
+        end,
+        condition = noice.api.statusline.search.has(),
+        highlight = "GalaxyBlue",
+    },
 }
 
 gls.short_line_left[1] = {
-	ViMode = {
-		provider = function()
-			return vi_mode()
-		end,
-		highlight = "GalaxyFg",
-		separator = " ",
-		separator_highlight = "GalaxyFgAlt",
-	},
+    ViMode = {
+        provider = function()
+            return vi_mode()
+        end,
+        highlight = "GalaxyFg",
+        -- separator = " ",
+        -- separator_highlight = "GalaxyFgAlt",
+    },
 }
 
 gls.short_line_left[3] = {
-	BufferType = {
-		provider = "FileTypeName",
-		highlight = "GalaxyFgAlt",
-	},
+    BufferType = {
+        provider = "FileTypeName",
+        icon = "  ",
+        highlight = "GalaxyFgAlt2",
+    },
 }
