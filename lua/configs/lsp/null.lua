3f8899b035ac9c6b2db9564626b955a3d8054a6b
lckdscl <lckdscl@bim.boats>
2023-10-16 21:12:19 +0100

    feat (none-ls): update sources

--- lua/configs/lsp/null.lua
+++ lua/configs/lsp/null.lua
@@ -1,4 +1,4 @@
-local present, null_ls = pcall(require, "none-ls")
+local present, null_ls = pcall(require, "null-ls")
 local navic = require("nvim-navic")
 if not present then
 	return
@@ -24,22 +24,54 @@ local on_attach = function(client, bufnr)
 end
 
 local sources = {
-	null_ls.builtins.formatting.prettierd,
+	null_ls.builtins.formatting.prettierd.with({
+		filetypes = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"vue",
+			"css",
+			"scss",
+			"less",
+			"html",
+			"json",
+			"jsonc",
+			"yaml",
+			"markdown",
+			"markdown.mdx",
+			"graphql",
+			"handlebars",
+			"quarto",
+		},
+	}),
 	null_ls.builtins.formatting.black,
+	null_ls.builtins.formatting.beautysh,
+	null_ls.builtins.formatting.bibclean,
+	null_ls.builtins.formatting.remark.with({
+		filetypes = { "markdown", "quarto" },
+	}),
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.formatting.latexindent.with({
+		args = { "-m", "-l", "~/downloads/textwrap.yaml", "-" },
+	}),
+	null_ls.builtins.hover.dictionary,
+	null_ls.builtins.formatting.cbfmt,
 	null_ls.builtins.diagnostics.chktex,
 	null_ls.builtins.diagnostics.cppcheck,
 	null_ls.builtins.diagnostics.codespell,
 	null_ls.builtins.diagnostics.vint,
 	null_ls.builtins.diagnostics.revive,
-	null_ls.builtins.diagnostics.proselint,
+	null_ls.builtins.diagnostics.proselint.with({
+		filetypes = { "markdown", "tex", "quarto" },
+	}),
 	null_ls.builtins.diagnostics.mypy,
 	null_ls.builtins.diagnostics.pylint,
 	null_ls.builtins.diagnostics.shellcheck,
-	null_ls.builtins.formatting.stylua,
-	null_ls.builtins.formatting.latexindent.with({
-		args = { "-m", "-l", "~/downloads/textwrap.yaml", "-" },
-	}),
 	null_ls.builtins.code_actions.gitsigns,
+	null_ls.builtins.code_actions.proselint.with({
+		filetypes = { "markdown", "tex", "quarto" },
+	}),
 }
 
 null_ls.setup({
