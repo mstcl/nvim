39b915c11df3e4cf81180c3069dc3e10ccc0eaba
lckdscl <lckdscl@bim.boats>
2023-10-17 22:12:58 +0100

    feat (ts): add textobjects plugin

--- lua/configs/syntax/treesitter.lua
+++ lua/configs/syntax/treesitter.lua
@@ -68,7 +68,7 @@ ts.setup({
 	playground = {
 		enable = true,
 		disable = {},
-		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		updatetime = 25,   -- Debounced time for highlighting nodes in the playground from source code
 		persist_queries = false, -- Whether the query persists across vim sessions
 		keybindings = {
 			toggle_query_editor = "o",
@@ -83,6 +83,63 @@ ts.setup({
 			show_help = "?",
 		},
 	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["ak"] = { query = "@block.outer", desc = "around block" },
+				["ik"] = { query = "@block.inner", desc = "inside block" },
+				["ac"] = { query = "@class.outer", desc = "around class" },
+				["ic"] = { query = "@class.inner", desc = "inside class" },
+				["a?"] = { query = "@conditional.outer", desc = "around conditional" },
+				["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
+				["af"] = { query = "@function.outer", desc = "around function " },
+				["if"] = { query = "@function.inner", desc = "inside function " },
+				["al"] = { query = "@loop.outer", desc = "around loop" },
+				["il"] = { query = "@loop.inner", desc = "inside loop" },
+				["aa"] = { query = "@parameter.outer", desc = "around argument" },
+				["ia"] = { query = "@parameter.inner", desc = "inside argument" },
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]k"] = { query = "@block.outer", desc = "Next block start" },
+				["]f"] = { query = "@function.outer", desc = "Next function start" },
+				["]a"] = { query = "@parameter.inner", desc = "Next parameter start" },
+			},
+			goto_next_end = {
+				["]K"] = { query = "@block.outer", desc = "Next block end" },
+				["]F"] = { query = "@function.outer", desc = "Next function end" },
+				["]A"] = { query = "@parameter.inner", desc = "Next parameter end" },
+			},
+			goto_previous_start = {
+				["[k"] = { query = "@block.outer", desc = "Previous block start" },
+				["[f"] = { query = "@function.outer", desc = "Previous function start" },
+				["[a"] = { query = "@parameter.inner", desc = "Previous parameter start" },
+			},
+			goto_previous_end = {
+				["[K"] = { query = "@block.outer", desc = "Previous block end" },
+				["[F"] = { query = "@function.outer", desc = "Previous function end" },
+				["[A"] = { query = "@parameter.inner", desc = "Previous parameter end" },
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				[">K"] = { query = "@block.outer", desc = "Swap next block" },
+				[">F"] = { query = "@function.outer", desc = "Swap next function" },
+				[">A"] = { query = "@parameter.inner", desc = "Swap next parameter" },
+			},
+			swap_previous = {
+				["<K"] = { query = "@block.outer", desc = "Swap previous block" },
+				["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+				["<A"] = { query = "@parameter.inner", desc = "Swap previous parameter" },
+			},
+		},
+	},
 })
 
-require('utils.autocmds.treesitter')
+require("utils.autocmds.treesitter")
