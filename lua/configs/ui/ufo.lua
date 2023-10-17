b79612c7906ceebe76afc878ec66a45c886d33ca
lckdscl <lckdscl@bim.boats>
2023-10-17 22:13:59 +0100

    feat (ufo): logic to handle fallback

--- lua/configs/ui/ufo.lua
+++ lua/configs/ui/ufo.lua
@@ -47,6 +47,26 @@ ufo.setup({
         },
     },
     fold_virt_text_handler = handler,
+    provider_selector = function(_, filetype, buftype)
+        local function handleFallbackException(bufnr, err, providerName)
+            if type(err) == "string" and err:match("UfoFallbackException") then
+                return require("ufo").getFolds(bufnr, providerName)
+            else
+                return require("promise").reject(err)
+            end
+        end
+        return (filetype == "" or buftype == "nofile") and "indent"
+            or function(bufnr)
+                return require("ufo")
+                    .getFolds(bufnr, "lsp")
+                    :catch(function(err)
+                        return handleFallbackException(bufnr, err, "treesitter")
+                    end)
+                    :catch(function(err)
+                        return handleFallbackException(bufnr, err, "indent")
+                    end)
+            end
+    end,
 })
 
 vim.keymap.set("n", "K", function()
