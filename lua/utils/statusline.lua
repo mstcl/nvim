0854edcb8c9fe19c4606ab58f48957a26ce9de8c
lckdscl <lckdscl@bim.boats>
2023-10-24 23:02:14 +0100

    feat (statusline): some updates to looks and functionality

--- lua/utils/statusline.lua
+++ lua/utils/statusline.lua
@@ -1,188 +1,293 @@
 local gl = require("galaxyline")
 local noice = require("noice")
 local condition = require("galaxyline.condition")
-local fileinfo = require('galaxyline.provider_fileinfo')
+local buffer = require("galaxyline.provider_buffer")
+local vcs = require("galaxyline.provider_vcs")
 local gls = gl.section
 local vi_mode = require("utils.misc").statusline_vi_mode
 
 gl.short_line_list = require("user_configs").statusline_short_ft
 
+gls.left[1] = {
+	ViMode = {
+		provider = require("utils.misc").statusline_vi_mode,
+		highlight = "GalaxyFg",
+	},
+}
+
+-- gls.left[2] = {
+-- 	FileName = {
+-- 		provider = function()
+-- 			return fileinfo.get_current_file_name("●", "✗")
+-- 		end,
+-- 		condition = condition.buffer_not_empty,
+-- 		highlight = "GalaxyFgAlt",
+-- 		icon = " ",
+-- 	},
+-- }
+
 gls.left[2] = {
-    ViMode = {
-        provider = require("utils.misc").statusline_vi_mode,
-        highlight = "GalaxyFg",
-    },
+	FileFormat = {
+		provider = function()
+			return buffer.get_buffer_filetype()
+		end,
+		highlight = "GalaxyFgAlt",
+		icon = "  ",
+		separator_highlight = "GalaxyFgAlt",
+		separator = " ",
+	},
+}
+
+-- gls.left[3] = {
+-- 	GitTag = {
+-- 		provider = function()
+-- 			return "GIT "
+-- 		end,
+-- 		condition = condition.check_git_workspace,
+-- 		highlight = "GalaxyFgAlt2I",
+-- 		separator_highlight = "GalaxyFgAlt2I",
+-- 		icon = " "
+-- 	}
+-- }
+
+-- gls.left[7] = {
+-- 	BranchSpace = {
+-- 		provider = function()
+-- 			return " "
+-- 		end,
+-- 		highlight = "GalaxyFgAlt2",
+-- 	},
+-- }
+
+gls.left[4] = {
+	GitBranch = {
+		provider = function()
+			if vcs.get_git_branch() == nil then
+				return ""
+			else
+				return "[" .. vcs.get_git_branch() .. "]"
+			end
+		end,
+		condition = condition.check_git_workspace,
+		highlight = "GalaxyFgAlt2",
+		icon = " ",
+		-- separator_highlight = "GalaxyFgAlt2",
+		-- separator = " ",
+	},
 }
 
 gls.left[5] = {
-    FileName = {
-        provider = function()
-            return fileinfo.get_current_file_name("●","✗")
-        end,
-        condition = condition.buffer_not_empty,
-        highlight = "GalaxyFgAlt",
-        icon = " ",
-    },
+	DiffAdd = {
+		provider = "DiffAdd",
+		icon = "  +",
+		highlight = "GalaxyGreen",
+		separator_highlight = "GalaxyFgAlt2",
+		separator = "",
+	},
 }
 
-gls.left[9] = {
-    BranchSpace = {
-        provider = function()
-            return " "
-        end,
-        highlight = "GalaxyFgAlt2",
-    },
+gls.left[6] = {
+	DiffModified = {
+		provider = "DiffModified",
+		icon = "  ~",
+		highlight = "GalaxyMagenta",
+		separator_highlight = "GalaxyFgAlt2",
+		separator = "",
+	},
 }
 
-gls.left[10] = {
-    DiffAdd = {
-        provider = "DiffAdd",
-        icon = " +",
-        highlight = "GalaxyGreen",
-    },
+gls.left[7] = {
+	DiffRemove = {
+		provider = vcs.diff_remove,
+		icon = "  -",
+		highlight = "GalaxyRed",
+		separator_highlight = "GalaxyFgAlt2",
+		separator = "",
+	},
 }
 
-gls.left[11] = {
-    DiffModified = {
-        provider = "DiffModified",
-        icon = " ~",
-        highlight = "GalaxyMagenta",
-    },
+gls.left[8] = {
+	Extra1 = {
+		provider = function()
+			if vcs.diff_remove() == nil and vcs.diff_add() == nil and vcs.diff_modified() == nil then
+				return ""
+			else
+				return " "
+			end
+		end,
+		separator_highlight = "GalaxyFgAlt2",
+		highlight = "GalaxyFgAlt2",
+		-- condition = condition.check_git_workspace,
+		-- separator = " ",
+	},
 }
 
 gls.left[12] = {
-    DiffRemove = {
-        provider = "DiffRemove",
-        icon = " -",
-        highlight = "GalaxyRed",
-    },
+	LspTag = {
+		provider = function()
+			if condition.check_git_workspace() then
+				return ""
+			else
+				return " "
+			end
+		end,
+		highlight = "GalaxyFgAlt2",
+	},
 }
 
 gls.left[13] = {
-    GitIcon = {
-        provider = function()
-            return " ∫"
-        end,
-        condition = condition.check_git_workspace,
-        separator = " ",
-        separator_highlight = "GalaxyFgAlt2",
-        highlight = "GalaxyFgAlt2",
-    },
+	LspClient = {
+		provider = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+			if next(clients) == nil then
+				return "[No client detected]"
+			end
+			local clients_str = "["
+			local first = true
+			local padding = ""
+			for _, client in ipairs(clients) do
+				if not first then
+					padding = " "
+				end
+				clients_str = clients_str .. padding .. client.name
+				if first then
+					first = false
+				end
+			end
+			return clients_str .. "]"
+		end,
+		highlight = "GalaxyFgAlt2",
+		-- icon = " ",
+		-- separator_highlight = "GalaxyFgAlt2",
+		-- separator = " ",
+	},
 }
 
 gls.left[14] = {
-    GitBranch = {
-        provider = "GitBranch",
-        condition = condition.check_git_workspace,
-        highlight = "GalaxyFgAlt2",
-    },
+	DiagnosticError = {
+		provider = "DiagnosticError",
+		icon = "  ✗",
+		highlight = "GalaxyRed",
+	},
 }
+
+gls.left[15] = {
+	DiagnosticWarn = {
+		provider = "DiagnosticWarn",
+		icon = "  !",
+		highlight = "GalaxyOrange",
+		-- separator_highlight = "GalaxyFgAlt2",
+		-- separator = " ",
+	},
+}
+
+gls.left[16] = {
+	DiagnosticHint = {
+		provider = "DiagnosticHint",
+		icon = "  ?",
+		highlight = "GalaxyBlue",
+		-- separator_highlight = "GalaxyFgAlt2",
+		-- separator = " ",
+	},
+}
+
+gls.left[17] = {
+	DiagnosticInfo = {
+		provider = "DiagnosticInfo",
+		icon = "  i",
+		highlight = "GalaxyMagenta",
+		-- separator_highlight = "GalaxyFgAlt2",
+		-- separator = " ",
+	},
+}
+
+gls.mid[18] = {
+	Macro = {
+		provider = noice.api.statusline.mode.get,
+		condition = noice.api.status.mode.has(),
+		highlight = "GalaxyFgAlt2",
+		-- icon = "  ",
+	},
+}
+
+-- gls.right[1] = {
+-- 	CwdTag = {
+-- 		provider = function()
+-- 			return " CWD "
+-- 		end,
+-- 		highlight = "GalaxyFgAlt2I",
+-- 		icon = " "
+-- 	}
+-- }
+
+gls.right[1] = {
+	VimOpts = {
+		provider = function()
+			local indent_mode = "TAB"
+			if vim.o.expandtab then
+				indent_mode = "SHIFT"
+			end
+			return "[" .. indent_mode .. ": " .. vim.o.shiftwidth .. "]"
+		end,
+		highlight = "GalaxyFgAlt2",
+		-- separator = " ",
+		-- separator_highlight = "GalaxyFgAlt2",
+	},
+}
+
+gls.right[2] = {
+	FilePath = {
+		provider = function()
+			return "[" .. vim.fn.getcwd() .. "]"
+		end,
+		condition = condition.buffer_not_empty,
+		highlight = "GalaxyFgAlt2",
+		-- separator = " ",
+		-- separator_highlight = "GalaxyFgAlt2",
+	},
+}
+
+-- gls.right[3] = {
+-- 	PaddingRight = {
+-- 		provider = function ()
+-- 			return " "
+-- 		end,
+-- 		highlight = "GalaxyFgAlt2",
+-- 	}
+-- }
 
 gls.mid[1] = {
-    FilePath = {
-        provider = function()
-            return vim.fn.getcwd()
-        end,
-        condition = condition.buffer_not_empty,
-        highlight = "GalaxyFgAlt2",
-    },
+	Search = {
+		provider = noice.api.statusline.search.get,
+		condition = noice.api.statusline.search.has(),
+		highlight = "GalaxyFgAlt2",
+		separator = " ",
+		separator_highlight = "GalaxyFgAlt2",
+	},
 }
 
-gls.right[7] = {
-    DiagnosticError = {
-        provider = "DiagnosticError",
-        icon = " ✗",
-        highlight = "GalaxyRed",
-    },
-}
-
-gls.right[8] = {
-    DiagnosticWarn = {
-        provider = "DiagnosticWarn",
-        icon = " !",
-        highlight = "GalaxyOrange",
-    },
-}
-
-gls.right[9] = {
-    DiagnosticHint = {
-        provider = "DiagnosticHint",
-        icon = " ?",
-        highlight = "GalaxyYellow",
-    },
-}
-
-gls.right[10] = {
-    DiagnosticInfo = {
-        provider = "DiagnosticInfo",
-        icon = " i",
-        highlight = "GalaxyBlue",
-    },
-}
-
-gls.right[13] = {
-    LineInfo = {
-        provider = "LineColumn",
-        separator = "",
-        separator_highlight = "GalaxyFgAlt2",
-        highlight = "GalaxyFgAlt2",
-        icon = "⊕ ",
-    },
-}
-
-gls.right[14] = {
-    LinePercent = {
-        separator = "@",
-        separator_highlight = "GalaxyFgAlt2",
-        provider = "LinePercent",
-        highlight = "GalaxyFgAlt2",
-    },
-}
-
-gls.right[15] = {
-    Macro = {
-        provider = function()
-            return noice.api.statusline.mode.get()
-        end,
-        condition = noice.api.status.mode.has(),
-        icon = " ❖ ",
-        highlight = "GalaxyGreen",
-    },
-}
-
-gls.right[16] = {
-    Whitespace = {
-        provider = "WhiteSpace",
-        icon = " ⇆ ",
-        separator = "",
-        separator_highlight = "GalaxyFgAlt2",
-        highlight = "GalaxyYellow",
-    },
-}
-
-gls.right[17] = {
-    Search = {
-        provider = function()
-            return noice.api.statusline.search.get()
-        end,
-        condition = noice.api.statusline.search.has(),
-        highlight = "GalaxyBlue",
-    },
+gls.mid[3] = {
+	Whitespace = {
+		provider = "WhiteSpace",
+		highlight = "GalaxyFgAlt2",
+		-- icon = "⇆ ",
+		separator = " ",
+		separator_highlight = "GalaxyFgAlt2",
+	},
 }
 
 gls.short_line_left[1] = {
-    ViMode = {
-        provider = function()
-            return vi_mode()
-        end,
-        highlight = "GalaxyFg",
-    },
+	ViMode = {
+		provider = vi_mode,
+		highlight = "GalaxyFg",
+	},
 }
 
 gls.short_line_left[3] = {
-    BufferType = {
-        provider = "FileTypeName",
-        icon = "  ",
-        highlight = "GalaxyFgAlt2",
-    },
+	BufferType = {
+		provider = "FileTypeName",
+		icon = "  ",
+		highlight = "GalaxyFgAlt2",
+	},
 }
