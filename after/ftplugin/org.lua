e04ad401d42326dae678c3e40d835b42c4d1d659
lckdscl <lckdscl@bim.boats>
2023-10-28 00:17:56 +0100

    chores (ftplugin): add typst and org

---
+++ after/ftplugin/org.lua
@@ -0,0 +1,9 @@
+local setlocal = vim.opt_local
+
+setlocal.shiftwidth = 4
+setlocal.expandtab = true
+setlocal.expandtab = true
+setlocal.conceallevel = 2
+setlocal.concealcursor = "nc"
+setlocal.shiftwidth = 2
+setlocal.foldlevel = 99
