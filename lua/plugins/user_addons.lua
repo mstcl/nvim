a0afabf6aaf0faf161d103a93ce4f2f10cd691e1
lckdscl <lckdscl@bim.boats>
2024-03-10 17:21:55 +0000

    chore: user custom

--- lua/plugins/user_addons.lua
+++ lua/plugins/user_addons.lua
@@ -1,5 +1,12 @@
 -- Additional addons go here
 return {
-	"rktjmp/lush.nvim",
-	"rktjmp/shipwright.nvim",
+	-- "rktjmp/lush.nvim",
+	-- "rktjmp/shipwright.nvim",
+	{
+		"nvim-orgmode/orgmode",
+		opts = {
+			org_capture_templates = { t = { description = "New task", template = "* TODO %?\n  %u" } },
+			org_default_notes_file = "~/sftpgo/notes/Main.org",
+		},
+	},
 }
