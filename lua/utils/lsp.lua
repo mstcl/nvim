628973cf0c70aedead207198c2e427279060d26b
lckdscl <lckdscl@bim.boats>
2024-04-27 17:15:46 +0100

    merge: master

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -1,18 +1,5 @@
-local ufo_ok, ufo = pcall(require, "ufo")
-if not ufo_ok then
-	return
-end
-local null_ok, null_ls = pcall(require, "null-ls")
-if not null_ok then
-	return
-end
 local vl = vim.lsp
 local M = {}
-local null_sources = {}
-local fmt_sources = require("user_configs").null_formatting_sources
-local hover_sources = require("user_configs").null_hover_sources
-local diagnostics_sources = require("user_configs").null_diagnostics_sources
-local code_action_sources = require("user_configs").null_code_action_sources
 
 function M.on_attach(client, bufnr)
 	require("utils.mappings.lsp").setup(client, bufnr)
@@ -52,62 +39,5 @@ M.handlers = {
 	["textDocument/hover"] = vl.with(vl.handlers.hover, { border = "single" }),
 	["textDocument/publishDiagnostics"] = vl.with(vl.diagnostic.on_publish_diagnostics, { update_in_insert = false }),
 }
-
-for _, source in ipairs(fmt_sources) do
-	if source == "mdformat" then
-		table.insert(
-			null_sources,
-			null_ls.builtins.formatting[source].with({
-				filetypes = { "markdown", "quarto" },
-			})
-		)
-	end
-	if source == "cbfmt" then
-		table.insert(
-			null_sources,
-			null_ls.builtins.formatting[source].with({
-				filetypes = { "markdown", "quarto", "org" },
-			})
-		)
-	end
-	table.insert(null_sources, null_ls.builtins.formatting[source])
-end
-for _, source in ipairs(hover_sources) do
-	table.insert(null_sources, null_ls.builtins.hover[source])
-end
-for _, source in ipairs(diagnostics_sources) do
-	if source == "commitlint" then
-		table.insert(
-			null_sources,
-			null_ls.builtins.diagnostics[source].with({
-				filetypes = { "NeogitCommitMessage", "gitcommit" },
-				env = { NODE_PATH = vim.fn.expand("$HOME/.local/share/nvim/mason/packages/commitlint/node_modules") },
-				extra_args = { "--extends", "@commitlint/config-conventional" },
-			})
-		)
-	end
-	if source == "proselint" then
-		table.insert(
-			null_sources,
-			null_ls.builtins.diagnostics[source].with({
-				filetypes = { "markdown", "quarto", "org", "tex" },
-			})
-		)
-	end
-	table.insert(null_sources, null_ls.builtins.diagnostics[source])
-end
-for _, source in ipairs(code_action_sources) do
-	if source == "proselint" then
-		table.insert(
-			null_sources,
-			null_ls.builtins.code_actions[source].with({
-				filetypes = { "markdown", "quarto", "org", "tex" },
-			})
-		)
-	end
-	table.insert(null_sources, null_ls.builtins.code_actions[source])
-end
-
-M.null_sources = null_sources
 
 return M
