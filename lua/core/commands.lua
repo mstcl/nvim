9b94b302b0d9008e2cf7b48b5b3d038909feca38
sleepingcat <sleepingcat@lonely-desk.top>
2024-12-07 23:33:03 +0000

    chore: resolve most annoying breaking changes

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,5 +1,4 @@
 local create = vim.api.nvim_create_user_command
 create("WipeReg", "for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor", { bang = false })
-create("E", "Oil --float", {})
-create("Explore", "Oil --float", {})
+create("E", "Oil", {})
 create("Msg", "lua require('mini.notify').show_history()", {})
