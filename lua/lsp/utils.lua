ef99d826f7eff36766e89d092fcc198ec65d3b20
sleepingcat <sleepingcat@lonely-desk.top>
2025-08-10 12:29:16 +0100

    chore: no need to inject blink capabilities with v0.11

--- lua/lsp/utils.lua
+++ lua/lsp/utils.lua
@@ -118,7 +118,6 @@ end
 -- Override default capabilities
 function M.create_capabilities()
 	local capabilities = lsp.protocol.make_client_capabilities()
-	capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities({}, false))
 	capabilities = vim.tbl_deep_extend("force", capabilities, {
 		textDocument = {
 			foldingRange = {
