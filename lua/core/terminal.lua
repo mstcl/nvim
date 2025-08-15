23501ad2566c3baf5cbc9b8fd4ee1638ed8f6f97
code <code@beee.ps>
2025-08-15 21:59:26 +0100

    fix: terminal scoped to tabpage

--- lua/core/terminal.lua
+++ lua/core/terminal.lua
@@ -2,44 +2,41 @@ local exec = vim.api.nvim_command
 
 local M = {}
 
-local t_buf = nil
-local t_win_id = nil
-
 local function split_new_terminal()
-	if vim.api.nvim_win_get_width(0) >= 150 then
-		exec("vsplit | winc L | vertical resize 50 | term ")
+	if vim.api.nvim_win_get_width(0) >= 350 then
+		exec("vsplit | winc L | vertical resize 150 | term")
 	else
 		exec("split | winc J | resize 10 | term")
 	end
 end
 
 local function split_existing_terminal()
-	if vim.api.nvim_win_get_width(0) >= 150 then
-		exec("vert sb " .. t_buf .. "| winc L | vertical resize 50")
+	if vim.api.nvim_win_get_width(0) >= 350 then
+		exec("vert sb " .. vim.t.t_buf .. "| winc L | vertical resize 150")
 	else
-		exec("sb" .. t_buf .. "| winc J | resize 10")
+		exec("sb" .. vim.t.t_buf .. "| winc J | resize 10")
 	end
 end
 
 local function open_terminal()
-	if vim.fn.bufexists(t_buf) ~= 1 then
+	if vim.fn.bufexists(vim.t.t_buf) ~= 1 then
 		split_new_terminal()
-		t_win_id = vim.fn.win_getid()
-		t_buf = vim.fn.bufnr("%")
-	elseif vim.fn.win_gotoid(t_win_id) ~= 1 then
+		vim.t.t_win_id = vim.fn.win_getid()
+		vim.t.t_buf = vim.fn.bufnr("%")
+	elseif vim.fn.win_gotoid(vim.t.t_win_id) ~= 1 then
 		split_existing_terminal()
-		t_win_id = vim.fn.win_getid()
+		vim.t.t_win_id = vim.fn.win_getid()
 	end
 end
 
 local function hide_terminal()
-	if vim.fn.win_gotoid(t_win_id) == 1 then
+	if vim.fn.win_gotoid(vim.t.t_win_id) == 1 then
 		exec("hide")
 	end
 end
 
 function M.Toggle()
-	if vim.fn.win_gotoid(t_win_id) == 1 then
+	if vim.fn.win_gotoid(vim.t.t_win_id) == 1 then
 		hide_terminal()
 	else
 		open_terminal()
