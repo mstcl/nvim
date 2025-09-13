260589337b136a7f04cfbd9edb9d97c6cc746ac2
code <code@beee.ps>
2025-09-13 17:35:21 +0100

    fix: commit custom zoxide picker

---
+++ lua/pickers/zoxide.lua
@@ -0,0 +1,36 @@
+local fzf_lua = require("fzf-lua")
+
+local M = {}
+
+M.zoxide = function()
+	fzf_lua.fzf_exec("zoxide query -l", {
+		winopts = {
+			title = " Zoxide ",
+			title_pos = "center",
+		},
+		fzf_opts = {
+			["--no-multi"] = "",
+		},
+		actions = {
+			["ctrl-o"] = {
+				fn = function(selected)
+					require("oil").open(selected[1])
+				end,
+				desc = "open-oil",
+				header = "open oil in selected directory",
+			},
+			["ctrl-s"] = {
+				fn = function(selected)
+					fzf_lua.files({ cwd = selected[1] })
+				end,
+				desc = "search-files",
+				header = "search files in selected directory",
+			},
+			["default"] = function(selected)
+				vim.cmd("cd " .. selected[1])
+			end,
+		},
+	})
+end
+
+return M
