389061b22e8e0e271751becf0744f3fb7d2497ce
code <code@beee.ps>
2025-09-21 11:28:16 +0100

    fix: weird

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -1,7 +1,7 @@
 ---@type vim.lsp.Config
 return {
 	cmd = { "yaml-language-server", "--stdio" },
-	{ "yaml", "yaml.docker-compose", "yaml.gitlab", "yaml.helm-values" },
+	filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab", "yaml.helm-values" },
 	root_markers = { ".git" },
 	on_init = function(client)
 		client.server_capabilities.documentFormattingProvider = true
