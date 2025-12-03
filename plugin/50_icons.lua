8c5732c054612e18e857c790bd3cfb62bab0b87e
code <code@beee.ps>
2025-12-03 23:39:13 +0000

    fix: move ui to icons
    needs to go before statusline as a requirement

---
+++ plugin/50_icons.lua
@@ -0,0 +1,2 @@
+-- (nvim-web-devicons) Icons
+MiniDeps.now(function() MiniDeps.add("nvim-tree/nvim-web-devicons") end)
--- plugin/50_ui.lua
+++
@@ -1,6 +0,0 @@
--- (nvim-web-devicons) Icons
-MiniDeps.now(function()
-	MiniDeps.add("nvim-tree/nvim-web-devicons")
-
-	require("nvim-web-devicons").setup({ variant = "light" })
-end)
