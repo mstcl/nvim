4b39a9b26ce3b70147d45f1b8a3319d3cea18f6d
code <code@beee.ps>
2025-12-03 23:36:42 +0000

    fix: move plugins to extras

--- plugin/60_extras.lua
+++ plugin/60_extras.lua
@@ -1,5 +1,49 @@
 local _plugin_path = vim.fn.stdpath("data") .. "/site/pack/deps/opt"
 
+-- Colorschemes
+MiniDeps.now(function()
+	MiniDeps.add("rktjmp/lush.nvim")
+	MiniDeps.add("mstcl/tavern.nvim")
+	MiniDeps.add("mstcl/ivory.nvim")
+end)
+
+-- (mini.notify) Popup notifications
+MiniDeps.now(function()
+	MiniDeps.add("nvim-mini/mini.notify")
+
+	require("mini.notify").setup({
+		content = {
+			format = function(notif)
+				return " " .. "[" .. notif.level .. "]" .. " " .. notif.msg .. " "
+			end,
+		},
+		lsp_progress = { enable = false },
+		window = {
+			max_width_share = 1,
+			winblend = 0,
+			config = { border = _G.config.border },
+		},
+	})
+
+	local vim_notify_opts = {
+		ERROR = { duration = 5000, hl_group = "DiagnosticOk" },
+		WARN = { duration = 5000, hl_group = "DiagnosticOk" },
+		INFO = { duration = 2000, hl_group = "DiagnosticOk" },
+		DEBUG = { duration = 0, hl_group = "DiagnosticOk" },
+		TRACE = { duration = 0, hl_group = "DiagnosticOk" },
+		OFF = { duration = 0, hl_group = "DiagnosticSignOk" },
+	}
+
+	vim.notify = require("mini.notify").make_notify(vim_notify_opts)
+
+	vim.keymap.set(
+		"n",
+		"<leader>N",
+		function() require("mini.notify").clear() end,
+		{ desc = "Dismiss all notifications", noremap = false, silent = true }
+	)
+end)
+
 -- (mini.clue) Mapping helper
 MiniDeps.later(function()
 	MiniDeps.add("nvim-mini/mini.clue")
@@ -251,6 +295,13 @@ MiniDeps.later(function()
 			update_n_lines = "gsn",
 		},
 	})
+end)
+
+-- (remember.nvim) Remember last place
+MiniDeps.now(function()
+	MiniDeps.add("vladdoster/remember.nvim")
+
+	require("remember")
 end)
 
 -- (oil.nvim) Buffer-like file browser
