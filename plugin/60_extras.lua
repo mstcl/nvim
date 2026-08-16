04e2c4d27b128d1ec7dabeab6233c77f77d9afdb
code <code@beee.ps>
2026-08-16 10:41:59 +0100

    don't disable signcolumn in neogit

--- plugin/60_extras.lua
+++ plugin/60_extras.lua
@@ -693,8 +693,6 @@ _G.later(function()
 				vim.b.indent_guide = false
 				vim.wo.statuscolumn = ""
 				vim.opt_local.foldcolumn = "0"
-				vim.opt_local.signcolumn = "no"
-				vim.wo.cursorline = false
 				vim.wo.colorcolumn = ""
 			end,
 		},
