1148f8472685c10ece6c7a88dd81206f29e40249
code <code@beee.ps>
2026-08-31 15:05:00 +0100

    custom surround for markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,3 +3,18 @@ local setlocal = vim.opt_local
 setlocal.shiftwidth = 4
 setlocal.expandtab = true
 setlocal.colorcolumn = ""
+
+vim.b.minisurround_config = {
+	custom_surroundings = {
+		L = {
+			input = { "%[().-()%]%(.-%)" },
+			output = function()
+				local link = require("mini.surround").user_input("Link")
+				return { left = "[", right = "](" .. link .. ")" }
+			end,
+		},
+		B = { output = { left = "**", right = "**" } },
+		I = { output = { left = "*", right = "*" } },
+		S = { output = { left = "~~", right = "~~" } },
+	},
+}
