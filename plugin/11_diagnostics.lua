146600c9b5fb5ab3d73613614dbf88dcb173e80b
code <code@beee.ps>
2026-08-31 10:31:19 +0100

    defer loading diagnostic config

--- plugin/11_diagnostics.lua
+++ plugin/11_diagnostics.lua
@@ -1,58 +1,61 @@
 ---@diagnostic disable-next-line: param-type-not-match
 -- Configure builtin diagnostics
-local lsp_status_signs = {
-	[vim.diagnostic.severity.ERROR] = config.signs.lsp.Error,
-	[vim.diagnostic.severity.WARN] = config.signs.lsp.Warn,
-	[vim.diagnostic.severity.INFO] = config.signs.lsp.Info,
-	[vim.diagnostic.severity.HINT] = config.signs.lsp.Hint,
-}
 
-local lsp_status_hl_map = {
-	[vim.diagnostic.severity.ERROR] = "DiagnosticError",
-	[vim.diagnostic.severity.WARN] = "DiagnosticWarn",
-	[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
-	[vim.diagnostic.severity.HINT] = "DiagnosticHint",
-}
+_G.helpers.later(function()
+	local lsp_status_signs = {
+		[vim.diagnostic.severity.ERROR] = config.signs.lsp.Error,
+		[vim.diagnostic.severity.WARN] = config.signs.lsp.Warn,
+		[vim.diagnostic.severity.INFO] = config.signs.lsp.Info,
+		[vim.diagnostic.severity.HINT] = config.signs.lsp.Hint,
+	}
 
-vim.diagnostic.config({
-	---@type vim.diagnostic.Opts
-	virtual_lines = false,
-	virtual_text = false,
-	underline = false,
-	update_in_insert = false,
-	severity_sort = true,
+	local lsp_status_hl_map = {
+		[vim.diagnostic.severity.ERROR] = "DiagnosticError",
+		[vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+		[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+		[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+	}
 
-	signs = {},
+	vim.diagnostic.config({
+		---@type vim.diagnostic.Opts
+		virtual_lines = false,
+		virtual_text = false,
+		underline = false,
+		update_in_insert = false,
+		severity_sort = true,
 
-	status = {
-		format = function(severity_counts)
-			local items = {}
-			for severity in ipairs(vim.diagnostic.severity) do
-				local count = severity_counts[severity] or 0
-				if count ~= 0 then
-					table.insert(
-						items,
-						("%%#%s#%s:%s"):format(
-							lsp_status_hl_map[severity],
-							lsp_status_signs[severity],
-							count
+		signs = {},
+
+		status = {
+			format = function(severity_counts)
+				local items = {}
+				for severity in ipairs(vim.diagnostic.severity) do
+					local count = severity_counts[severity] or 0
+					if count ~= 0 then
+						table.insert(
+							items,
+							("%%#%s#%s:%s"):format(
+								lsp_status_hl_map[severity],
+								lsp_status_signs[severity],
+								count
+							)
 						)
-					)
+					end
 				end
-			end
-			return table.concat(items, " ")
-		end,
-	},
-
-	float = {
-		close_events = {
-			"BufLeave",
-			"CursorMoved",
-			"InsertEnter",
-			"FocusLost",
+				return table.concat(items, " ")
+			end,
 		},
-		border = config.border,
-		source = "always",
-		focus = false,
-	},
-})
+
+		float = {
+			close_events = {
+				"BufLeave",
+				"CursorMoved",
+				"InsertEnter",
+				"FocusLost",
+			},
+			border = config.border,
+			source = "always",
+			focus = false,
+		},
+	})
+end)
