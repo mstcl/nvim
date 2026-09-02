31fee535c006ed94f526502577ec77a75da873d6
code <code@beee.ps>
2026-09-02 19:15:50 +0100

    fix ftplugin and autoindent fix

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,9 +1,3 @@
 local opt_local = vim.opt_local
 
 opt_local.expandtab = true
-
--- preserve indentation
-local map = vim.keymap.set
-map("i", "<CR>", "<CR>x<BS>", { noremap = true, silent = true })
-map("n", "o", "ox<BS>", { noremap = true, silent = true })
-map("n", "O", "Ox<BS>", { noremap = true, silent = true })
